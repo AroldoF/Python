f82fec4dcd35d7372bee385966fe3d6395cdf75f
Arôldo Fernandes <aroldo.caval@gmail.com>
2024-05-03 18:15:07 -0300

    Update

--- Questões/HelloWord.py
+++ Questões/HelloWord.py
@@ -1,1 +1,10 @@
 print("Hello Word!")
+print('7'+'4')
+nome='Aroldo'
+idade=18
+peso=63.3
+print(nome,idade,peso)
+nome=input('Qual o seu nome?')
+idade=input('Qual sua idade?')
+peso=input('Qual o Seu peso?')
+print(nome, idade, peso)

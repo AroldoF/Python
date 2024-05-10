6c4719124e5f68bd9e727739a771d0c8c5102d5a
Arôldo Fernandes <aroldo.caval@gmail.com>
2024-05-10 18:24:06 -0300

    colocando a musica

--- Questões/Ex021.py
+++ Questões/Ex021.py
@@ -1,6 +1,8 @@
 import pygame
+
 print("Sente o song")
 pygame.init()
-pygame.mixer.music.load("song.mp3")
-pygame.mixer.music.play()
+pygame.mixer.music.load("Questões/Ex021.mp3")
+pygame.mixer.music.play(loops=0, start=0.0)
 pygame.event.wait()
+input("Que vibes")
